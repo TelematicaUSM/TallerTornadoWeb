6456b6ac0bfbd6f6b243fccac0496c4ff7008af8
Cristobal Ganter <cganterh@gmail.com>
2016-04-20 12:45:26 -0300

    Se agrega validación del HTML al test de la parte 2 utilizando el servicio de https://validator.w3.org/nu/.

--- parte2/test.py
+++ parte2/test.py
@@ -1,4 +1,8 @@
-from tornado.testing import AsyncHTTPTestCase, main
+import json
+
+from tornado.httpclient import HTTPRequest
+from tornado.httputil import url_concat
+from tornado.testing import AsyncHTTPTestCase, gen_test, main
 
 from . import server
 
@@ -7,9 +11,12 @@ class TestApp(AsyncHTTPTestCase):
     def get_app(self):
         return server.new_app()
 
+    @gen_test
     def test_lista(self):
-        response = self.fetch('/lista')
-        self.assertEqual(response.code, 200, 'La respuesta no fué OK (200)')
+        url = self.get_url('/lista')
+        response = yield self.http_client.fetch(url)
+
+        self.assertEqual(response.code, 200, 'La respuesta no fue OK (200)')
         self.assertRegex(
             response.body.decode(),
             r'(?ms)'                            # Multiline and dot matches all
@@ -21,6 +28,23 @@ class TestApp(AsyncHTTPTestCase):
             'La respuesta no es una lista intercalada de checkboxes y '
             'etiquetas'
         )
+
+        validator_request = HTTPRequest(
+            url_concat('https://validator.w3.org/nu/', {'out': 'json'}),
+            'POST',
+            {'Content-Type': response.headers['Content-Type']},
+            response.body
+        )
+        validator_response = yield self.http_client.fetch(validator_request)
+        validator_results = json.loads(
+            validator_response.body.decode()
+        )
+        self.assertEqual(
+            len(validator_results['messages']),
+            0,
+            json.dumps(validator_results, indent=2)
+        )
+
 
 if __name__ == '__main__':
     main()
